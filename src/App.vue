164e2bf74006e8f17db186f7c57fe82200c0eae6
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-12-07 17:21:28 -0500

    Updates IE check

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,7 @@
             }
         },
         beforeMount() {
+            this.getBrowser();
             this.getMenu();
             this.getPages();
         },
@@ -61,6 +62,49 @@
             }
         },
         methods: {
+            getBrowser: function() {
+                var ua = window.navigator.userAgent;
+
+                // Test values; Uncomment to check result …
+
+                // IE 10
+                // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+                
+                // IE 11
+                // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+                
+                // Edge 12 (Spartan)
+                // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+                
+                // Edge 13
+                // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+                var msie = ua.indexOf('MSIE ');
+                if (msie > 0) {
+                    // IE 10 or older => return version number
+                    // return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+                    console.log('IE10 LESS');
+                }
+
+                var trident = ua.indexOf('Trident/');
+                if (trident > 0) {
+                    // IE 11 => return version number
+                    var rv = ua.indexOf('rv:');
+                    // return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+                    console.log('IE11');
+                }
+
+                var edge = ua.indexOf('Edge/');
+                if (edge > 0) {
+                    // Edge (IE 12+) => return version number
+                    // return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+                    console.log('EDGE');
+                }
+
+                // other browser
+                // return false;
+                console.log('OTHER');
+            },
             getMenu: function() {
               var app = this
 
