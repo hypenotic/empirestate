20f0aa6d976e3c814520f9f7bde24d484419fc14
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-11-23 10:25:03 -0500

    Update video comp code

--- src/components/videos.vue
+++ src/components/videos.vue
@@ -1,12 +1,11 @@
 <template>
     <section id="videos">
         <div class="embed-container" id="video-container">
-            <div class="video-wrap left" id="video-wrap-left"><iframe src="https://player.vimeo.com/video/302301981?muted=1&autoplay=1&loop=1" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen id="placeholder-vid-1" class=""></iframe></div>
+            <div class="video-wrap left move-item" id="video-wrap-left"><iframe src="https://player.vimeo.com/video/302301981?muted=1&autoplay=1&loop=1" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen id="placeholder-vid-1" class="move-item"></iframe></div>
 
-            <div class="video-wrap right" id="video-wrap-right"><iframe src="https://player.vimeo.com/video/302302610?muted=1" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen id="placeholder-vid-2"></iframe></div>
+            <div class="video-wrap right move-item" id="video-wrap-right"><iframe src="https://player.vimeo.com/video/302302610?muted=1" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen id="placeholder-vid-2" class="move-item"></iframe></div>
 
-            <svg width="16px" height="44px" id="right-arrow" v-on:click="moveVideo(true)" viewBox="0 0 16 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <defs></defs>
+            <svg width="16px" height="44px" id="right-arrow" v-on:click="moveVideo(true)" class="move-item" viewBox="0 0 16 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                     <g transform="translate(-1360.000000, -1227.000000)" class="arrow" fill-rule="nonzero" stroke="#FFFFFF" stroke-width="2">
                         <polyline points="1361 1270.41309 1374.19824 1249.02396 1361 1228"></polyline>
@@ -14,13 +13,14 @@
                 </g>
             </svg>
 
-            <svg width="16px" height="44px" viewBox="0 0 16 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="left-arrow" v-on:click="moveVideo(false)">
+            <svg width="16px" height="44px" viewBox="0 0 16 44" version="1.1" class="move-item" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="left-arrow" v-on:click="moveVideo(false)">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                     <g transform="translate(-1359.000000, -1227.000000)" fill-rule="nonzero" stroke="#FFFFFF" class="arrow" stroke-width="2">
                         <polyline transform="translate(1367.599121, 1249.206543) scale(-1, 1) translate(-1367.599121, -1249.206543) " points="1361 1270.41309 1374.19824 1249.02396 1361 1228"></polyline>
                     </g>
                 </g>
             </svg>
+
         </div>
     </section>
 </template>
@@ -54,21 +54,13 @@
         },
         methods: {
             moveVideo(status) {
+                var movingElements = document.getElementsByClassName('move-item');
+
+
                 if (status) {
-                    var vid1wrap = document.getElementById("video-wrap-left");
-                    vid1wrap.classList.add('move-back');
-                    var vid1 = document.getElementById("placeholder-vid-1");
-                    vid1.classList.add('move-back');
-                    var vid2wrap = document.getElementById("video-wrap-right");
-                    vid2wrap.classList.add('move-back');
-                    var vid2 = document.getElementById("placeholder-vid-2");
-                    vid2.classList.add('move-back');
-                    var cont = document.getElementById("videos");
-                    cont.classList.add('move-back');
-                    var right = document.getElementById("right-arrow");
-                    right.classList.add('move-back');
-                    var left = document.getElementById("left-arrow");
-                    left.classList.add('move-back');
+                    for (var i = 0; i < movingElements.length; i++) {
+                        movingElements[i].classList.add('move-active');
+                    }
 
                     var current = document.getElementById("placeholder-vid-1");
                     var player1 = new Vimeo.Player(current);
@@ -80,21 +72,11 @@
 
                     player2.setCurrentTime(0);
                     player2.play();
+
                 } else {
-                    var vid1wrap = document.getElementById("video-wrap-left");
-                    vid1wrap.classList.remove('move-back');
-                    var vid1 = document.getElementById("placeholder-vid-1");
-                    vid1.classList.remove('move-back');
-                    var vid2wrap = document.getElementById("video-wrap-right");
-                    vid2wrap.classList.remove('move-back');
-                    var vid2 = document.getElementById("placeholder-vid-2");
-                    vid2.classList.remove('move-back');
-                    var cont = document.getElementById("videos");
-                    cont.classList.remove('move-back');
-                    var right = document.getElementById("right-arrow");
-                    right.classList.remove('move-back');
-                    var left = document.getElementById("left-arrow");
-                    left.classList.remove('move-back');
+                    for (var i = 0; i < movingElements.length; i++) {
+                        movingElements[i].classList.remove('move-active');
+                    }
 
                     var current = document.getElementById("placeholder-vid-2");
                     var player1 = new Vimeo.Player(current);
@@ -106,7 +88,6 @@
                    
                     player2.setCurrentTime(0);
                     player2.play();
-                    // player2.setVolume(1);
                 }
                 
             },
@@ -121,23 +102,10 @@
 
 #videos {
     height: 85vh;
-    // background: lightgrey;
     margin-bottom: 80px;
     position: relative;
     overflow: hidden;
 }
-
-// #videos::after {
-//     display: block;
-//     position: absolute;
-//     background-image: linear-gradient(to right, black 0%, transparent 50%, black 100%);
-//     // margin-top: -80vh;
-//     top: 0;
-//     left: 0;
-//     height: 90vh;
-//     width: 100%;
-//     content: '';
-// }
 
 .embed-container { 
     position: relative; 
@@ -152,7 +120,6 @@
     position: relative;
     display: block;
     height: 100%;
-    // transition: left 0.5s ease;
     &:after {
         display: block;
         position: absolute;
@@ -166,23 +133,21 @@
         opacity: 1;
         transition: opacity 0.5s ease;
         width: 100%;
-        transition-delay: 1s;
+        transition-delay: 0.5s;
     }
-    &.right.move-back:after {
-        // display: none;
+    &.right.move-active:after {
         opacity: 0;
         width: 0;
         overflow: hidden;
     }
     &.left:after {
-        // display: none;
         opacity: 0;
         transition: opacity 0.5s ease;
-        transition-delay: 0.2s;
+        transition-delay: 0.5s;
         width: 0;
         overflow: hidden;
     }
-    &.left.move-back:after {
+    &.left.move-active:after {
         background-image: linear-gradient(to left, black 0%, transparent 50%, black 100%);
         top: 0;
         left: -83vw;
@@ -193,13 +158,10 @@
 }
     
 .embed-container >div {
-    // text-align: left;
     iframe#placeholder-vid-1 { 
         position: absolute; 
         top: 0; 
-        left: calc(-8vw); 
-        // object-position: center top;
-        // object-position: 100px 50px;
+        left: -8vw; 
         width: 100%; 
         height: 85vh; 
         transition: all 0.5s ease;
@@ -210,33 +172,18 @@
     position: absolute; 
     top: 0; 
     left: 73vw; 
-    // object-position: center top;
-    // object-position: 100px 50px;
     width: 100%; 
     height: 85vh; 
     transition: all 0.5s ease;
 }
 
-#placeholder-vid-1.move-back {
+#placeholder-vid-1.move-active {
     left: -73vw !important;
 }
 
-#placeholder-vid-2.move-back {
+#placeholder-vid-2.move-active {
     left: 8vw !important;
 }
-
-
-// #videos::after {
-//     display: block;
-//     position: absolute;
-//     background-image: linear-gradient(to right, black 0%, transparent 50%, black 100%);
-//     // margin-top: -80vh;
-//     top: 0;
-//     left: 80vw;
-//     height: 90vh;
-//     width: 100%;
-//     content: '';
-// }
 
 #right-arrow,
 #left-arrow {
@@ -256,8 +203,6 @@
 }
 
 #left-arrow {
-    // display: none;
-    // visibility: hidden;
     opacity: 0;
     display: block;
     position: absolute;
@@ -266,11 +211,12 @@
     z-index: 2000;
 }
 
-#left-arrow.move-back {
+#left-arrow.move-active {
     opacity: 1;
 }
 
-#right-arrow.move-back {
+#right-arrow.move-active {
     display: none !important;
 }
+
 </style>
